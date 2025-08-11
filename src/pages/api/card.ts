7c90f9ebcfe9fc1a15e7943a69e61e4ea05875a0
Francisco Borges <franciscomsborges2002@gmail.com>
2025-08-11 12:04:39 +0100

    docs: commenting /api/card flow

--- src/pages/api/card.ts
+++ src/pages/api/card.ts
@@ -18,31 +18,42 @@ export default async function handler(
   const url = `http://app:3000/card?${params}`;
 
   try {
+    //Connect to the Browserlerss Browser
     const browser = await puppeteer.connect({
       browserWSEndpoint: process.env.BROWSERLESS_URL || 'ws://localhost:5001',
-      //browserURL: "http://localhost:3001"
     });
 
+    //Create the new page
     const page = await browser.newPage();
     await page.setViewport(viewport);
     await page.goto(url, { waitUntil: 'networkidle0' });
 
+    //Go to the needed element
     const card = await page.$('#minimalCard');
+
+    // If there is no card, return an error
     if (!card) {
       await browser.close();
       return res.status(404).send('Card element not found');
     }
 
-    const buffer = await card.screenshot({ type: 'png' });
+    // Type of screenshot wanted
+    //Type:             png   -> Have radius border
+    //Fullpage:         false -> Just want the card element
+    //omitBackground:   true  -> Allow screenshots with transparancy
+    const buffer = await card.screenshot({ type: 'png', fullPage: false, omitBackground: true });
 
+    //Finally close the browser
     await browser.close();
 
     if (buffer) {
+      //Create the response
       res.setHeader('Content-Type', 'image/png');
       res.setHeader('Cache-Control', 'no-cache');
       return res.status(200).send(buffer);
     } else {
-      return res.status(500).json({ error: 'Could not capture screenshot' });
+      //Return an error message, if there is no image to be displayed
+      return res.status(500).json({ error: 'Could not get the image!' });//Screenshot could not be generated
     }
   } catch (err: unknown) {
     console.error('Puppeteer error:', err);
